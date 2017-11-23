fb1825baacf94a36f3cc54e95a82becbd85ff3d6
ziaochina <ziaochina@gmail.com>
2017-11-23 09:06:45 +0800

    zlj

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import appLoader from 'mk-app-loader'
+import * as appLoader from 'mk-app-loader'
 import utils from 'mk-utils'
-import component from 'mk-component'
+import * as component from 'mk-component'
 import metaEngine from 'mk-meta-engine'
 import 'mk-component/dist/assets/style/index.less'
 
