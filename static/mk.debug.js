2e8366b2489b426f9c6f6f935284acf8e88ca891
ziaochina <ziaochina@gmail.com>
2017-11-30 08:27:03 +0800

    zlj

--- static/mk.debug.js
+++ static/mk.debug.js
@@ -10,7 +10,7 @@ require.config({
         lodash: 'https://unpkg.com/lodash/lodash',
         immutable: 'https://unpkg.com/immutable/dist/immutable',
         echarts: 'https://unpkg.com/echarts/dist/echarts',
-        MK: 'https://unpkg.com/mk-sdk/dist/mk-core.min',
+        MK: 'https://unpkg.com/mk-sdk/dist/mk-core',
         'MK.appLoader': 'https://unpkg.com/mk-sdk/dist/mk-app-loader',
         'MK.utils': 'https://unpkg.com/mk-sdk/dist/mk-utils',
         'MK.component': 'https://unpkg.com/mk-sdk/dist/mk-component',
